585ed8ac6d8861eb3e1da0170f553813c010a1c7
Aleksandar Golubovic <agolubovicnn@gmail.com>
2017-07-20 10:56:32 +0200

    Add categories model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
+
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  resources :categories, except: [:destroy]
 end
